337008af7eaac5ef2690695abd778e26c6efc67b
sofau <sophiefauquembergue@gmail.com>
2024-11-07 12:40:23 +0100

    add tests scenarios when the user clicks on show/hide details btn

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,35 +1,70 @@
 // src/__tests__/Event.test.js
-import React from 'react';
-import EventList from '../components/EventList';
-import Event from '../components/Event';
+import React from "react";
+import Event from "../components/Event";
 import mockData from "../mock-data";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 const event = mockData[0];
 
-describe('<Event /> component', () => {
-    let EventComponent;
-    beforeEach(() => {
-        EventComponent = render(<Event event={event}/>);
-    });
-    
-    test('render event title', () => {
-        const eventTitle = EventComponent.queryByText(event.summary);
-        expect(eventTitle).toBeInTheDocument();
-    });
-
-    test('render event created date', () => {
-      const eventCreated = EventComponent.queryByText(event.created); 
-      expect(eventCreated).toBeInTheDocument();
+describe("<Event /> component", () => {
+  let EventComponent;
+  beforeEach(() => {
+    EventComponent = render(<Event event={event} />);
   });
 
-  test('render event location', () => {
-        const eventLocation = EventComponent.queryByText(event.location);
-        expect(eventLocation).toBeInTheDocument();
-    });
+  // Render component elements
+  test("render event title", () => {
+    const eventTitle = EventComponent.queryByText(event.summary);
+    expect(eventTitle).toBeInTheDocument();
+  });
 
-    test('render event details button', () => {
-      const detailButton = EventComponent.queryByText('Show Details');
-      expect(detailButton).toBeInTheDocument();
+  test("render event created date", () => {
+    const eventCreated = EventComponent.queryByText(event.created);
+    expect(eventCreated).toBeInTheDocument();
+  });
+
+  test("render event location", () => {
+    const eventLocation = EventComponent.queryByText(event.location);
+    expect(eventLocation).toBeInTheDocument();
+  });
+
+  // Show Details btn
+  test("render event details button", () => {
+    const detailButton = EventComponent.queryByText("Show Details");
+    expect(detailButton).toBeInTheDocument();
+  });
+
+  // Scenario 1
+  test("event details are hidden by default", () => {
+    const eventDetails = EventComponent.queryByText("About event:"); // Check if the event details heading is visible
+    expect(eventDetails).not.toBeInTheDocument();
+  });
+
+  // Scenario 2
+  test('shows event details when clicking "Show Details"', async () => {
+    const user = userEvent.setup();
+
+    const showDetailButton = EventComponent.queryByText("Show Details");
+    await user.click(showDetailButton);
+
+    const eventDetailsDom = EventComponent.container.firstChild;
+    const eventDetails = eventDetailsDom.querySelector(".event-details");
+    expect(eventDetails).toBeInTheDocument();
+  });
+
+  // Scenario 3
+  test('hide details when clicking on button "Hide details', async () => {
+    const user = userEvent.setup();
+
+    const showDetailButton = EventComponent.queryByText("Show Details");
+    await user.click(showDetailButton);
+
+    const hideDetailButton = EventComponent.queryByText("Hide Details");
+    await user.click(hideDetailButton);
+
+    const eventDetailsDom = EventComponent.container.firstChild;
+    const eventDetails = eventDetailsDom.querySelector(".eventDetails");
+    expect(eventDetails).not.toBeInTheDocument();
   });
 });
--- src/components/Event.js
+++ src/components/Event.js
@@ -5,23 +5,15 @@ import { useState } from "react";
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
 
-  // Toggle the button and details display
-  const toggleDetails = () => {
-    setShowDetails(!showDetails);
-  };
-
   return (
     <li>
       <div className="event">
         <h2>{event.summary}</h2>
         <p> {event.created} </p>
         <p> {event.location}</p>
-        <button onClick={toggleDetails}>
-          {showDetails ? "Hide Details" : "Show Details"}
-        </button>
-
-        {showDetails && (
-          <div className="event-details">
+        </div>
+      {showDetails ? (
+        <div className="event-details">
             <h3>About event:</h3>
             <a href={event.htmlLink} target="_blank" rel="noopener noreferrer">
               See details on Google Calendar
@@ -29,9 +21,13 @@ const Event = ({ event }) => {
             <p>
               <strong>Description:</strong> {event.description}
             </p>
-          </div>
-        )}
-      </div>
+            </div>
+      ) : null}
+      <button className="show-details-btn"
+        onClick={() => setShowDetails(!showDetails)}
+        >
+          {showDetails ? "Hide Details" : "Show Details"}
+        </button>
     </li>
   );
 };
