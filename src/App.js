afdf3fe42d4c8f3ed6beed50d10288725109bdf9
sofau <sophiefauquembergue@gmail.com>
2024-11-15 13:24:01 +0100

    set up infoAlert message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { InfoAlert } from './components/Alert';
 import { extractLocations, getEvents } from './api';
 import './App.css';
 
@@ -11,6 +12,7 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
 
   useEffect(() => {
@@ -33,14 +35,20 @@ const App = () => {
 
   return (
     <div className="App">
+      
       <CitySearch 
         allLocations={allLocations} 
-        setCurrentCity={setCurrentCity} /> 
-        {errorAlert && <div className="error-alert">{errorAlert}</div>} {/* Show error message if there's one */}
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert} />
+
       <NumberOfEvents 
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert} />
-        
+        {errorAlert && <div className="error-alert">{errorAlert}</div>} {/* Show error message if there's one */}
+      
+        <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+      </div>
       <EventList events={events} />
     </div>
   );
