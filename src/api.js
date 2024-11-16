680f417b493d98488b988c780d70008c819d86e4
sofau <sophiefauquembergue@gmail.com>
2024-11-16 15:45:57 +0100

    correct NProgress code

--- src/api.js
+++ src/api.js
@@ -21,15 +21,6 @@ export const extractLocations = (events) => {
  * @param {*} events - Array of events to process
  * @returns {Array} - Array of event objects with specific properties
  */
-export const getEventDetails = (events) => {
-  return events.map((event) => ({
-    summary: event.summary,
-    start: event.created,
-    location: event.location,
-    htmlLink: event.htmlLink,
-    description: event.description,
-  }));
-};
 
 const checkToken = async (accessToken) => {
   const response = await fetch(
@@ -39,12 +30,52 @@ const checkToken = async (accessToken) => {
   return result;
 };
 
-/**
- *
- * This function will fetch the list of all events
- */
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem("access_token");
+
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://2sccv6ladl.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
+
+const getToken = async (code) => {
+  try {
+    const encodeCode = encodeURIComponent(code);
+
+    const response = await fetch(
+      "https://2sccv6ladl.execute-api.eu-central-1.amazonaws.com/dev/api/token" +
+        "/" +
+        encodeCode
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+    return access_token;
+  } catch (error) {
+    error.json();
+  }
+};
+
 export const getEvents = async () => {
+  NProgress.start();
   if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
     return mockData;
   }
 
@@ -72,6 +103,16 @@ export const getEvents = async () => {
   }
 };
 
+export const getEventDetails = (events) => {
+  return events.map((event) => ({
+    summary: event.summary,
+    start: event.created,
+    location: event.location,
+    htmlLink: event.htmlLink,
+    description: event.description,
+  }));
+};
+
 const removeQuery = () => {
   let newurl;
   if (window.history.pushState && window.location.pathname) {
@@ -85,46 +126,4 @@ const removeQuery = () => {
     newurl = window.location.protocol + "//" + window.location.host;
     window.history.pushState("", "", newurl);
   }
-};
-
-const getToken = async (code) => {
-  try {
-    const encodeCode = encodeURIComponent(code);
-
-    const response = await fetch(
-      "https://2sccv6ladl.execute-api.eu-central-1.amazonaws.com/dev/api/token" +
-        "/" +
-        encodeCode
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const { access_token } = await response.json();
-    access_token && localStorage.setItem("access_token", access_token);
-    return access_token;
-  } catch (error) {
-    error.json();
-  }
-};
-
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem("access_token");
-
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const response = await fetch(
-        "https://2sccv6ladl.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const result = await response.json();
-      const { authUrl } = result;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
 };
